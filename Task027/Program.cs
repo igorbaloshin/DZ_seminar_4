5aebedb5615bddeaf2b2a5591d527e74f14ad828
igorbaloshin <iebaloshin@gmail.com>
2023-02-20 19:45:20 +0300

    решили задачи

---
+++ Task027/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int K = int.Parse(Console.ReadLine());
+//-----------------------------------------
+int CountNumbers(int n)
+{
+    int count = 0;
+    if(n < 0)
+       n *= (-1);
+    while(n > 0)
+    {
+        n /= 10; 
+        count++;
+    }
+    return count;
+} 
+//----------------------------------------
+void FillArray(int[] array, int K) // K число разлагаемое на массив
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = (K % Convert.ToInt32(Math.Pow(10, length - index)) - K % Convert.ToInt32(Math.Pow(10, (length - index - 1)))) / (Convert.ToInt32(Math.Pow(10, length - index - 1)));
+        index++;
+    }
+}
+//-------------------------------------------
+
+int Sum(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    int sum = 0;
+    while(index <= length -1)
+    {
+        sum = sum + array[index];
+        index++; 
+    }
+    return sum;
+}
+//-------------------------------------------
+int N = CountNumbers(K);
+
+int[] massiv = new int[N];
+
+FillArray(massiv, K);
+
+int sum = Sum(massiv);
+
+Console.WriteLine($"Сумма цифр числа = {sum}");
+
---
+++ Task029/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+void  FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 20);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+
+
+}
+ int[]array = new int[8];
+FillArray(array);
+PrintArray(array);
